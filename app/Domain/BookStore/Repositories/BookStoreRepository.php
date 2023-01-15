aa042fe231ae3cb20471a2153d6350ff3f48446e
Diego Pereira <san_diego19@hotmail.com>
2023-01-15 17:46:38 -0300

    feat: querybuilder - bonus implementation

--- app/Domain/BookStore/Repositories/BookStoreRepository.php
+++ app/Domain/BookStore/Repositories/BookStoreRepository.php
@@ -5,12 +5,21 @@ declare(strict_types=1);
 namespace App\Domain\BookStore\Repositories;
 
 use App\Domain\BookStore\Models\BookStore;
+use Spatie\QueryBuilder\AllowedFilter;
+use Spatie\QueryBuilder\QueryBuilder;
 
 class BookStoreRepository
 {
     public function listBooks()
     {
-        return BookStore::orderBy('book_id')->get();
+        return QueryBuilder::for(BookStore::class)
+            ->allowedFilters([
+                AllowedFilter::partial('book', 'name'),
+                AllowedFilter::exact('book_id'),
+            ])
+            ->defaultSort('book_id')
+            ->paginate(request('per_page', config('settings.AMOUNT_PAGINATE_DEFAULT')))
+            ->appends(request()->query());
     }
 
     public function createBooks(array $request): BookStore
