91d942fe178ebf8d5e2eb4e52f07ed163b89aeb2
Diego Pereira <san_diego19@hotmail.com>
2023-01-15 22:04:22 -0300

    feat: swagger implementation - bonus

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,6 +7,16 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ *@OA\Info(
+ * title="Book Store - API reference",
+ * version="1.0.0",
+ * description="Documentation of each EndPoint for integration.",
+ * @OA\Contact(
+ *  email="dhipereira21@gmail.com"
+ * ),
+ *),
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
